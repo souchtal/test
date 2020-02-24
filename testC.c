bfe42c2b9a4d421fecf5ed528787298250cc3293
Sawsane <sawsaneouchtal@gmail.com>
2020-02-24 15:08:39 +0100

    test commit

--- testC.c
+++ testC.c
@@ -1,6 +1,6 @@
 #include  <stdio.h>
- int main ()  {
- printf("lol");
-   printf("lo2l");
-   printf("test");  
-           }
+int main ()  {
+	printf("lol");
+  printf("lo2l");
+	printf("test");
+    } 
