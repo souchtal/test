deb41eb6e251e9c85e844056ac64189d4731317b
Sawsane <sawsaneouchtal@gmail.com>
2020-03-04 15:11:59 +0100

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,7 @@ html_logo  = '_static/fti_pic.jpeg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static'] 
+html_static_path = ['_static']  
 
 #added code 
 # def configureDoxyfile(input_dir, output_dir):
