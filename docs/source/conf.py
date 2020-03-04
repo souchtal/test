05cea70d1c59c9550f5738798576480c1b72e83e
Sawsane <sawsaneouchtal@gmail.com>
2020-03-04 12:49:04 +0100

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,7 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo  = '/home/bscuser/Documents/breathe/test/docs/source/_static/fti_pic.jpeg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
